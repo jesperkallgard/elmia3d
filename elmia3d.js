5eca457edf3634e3d010b08e19538f0e0002eb29
Jesper Källgård <jesperkallgard@gmail.com>
2018-01-15 13:24:00 +0100

    Added

--- elmia3d.js
+++ elmia3d.js
@@ -1,170 +1,137 @@
-window.elmia3d = {
-  camera : '',
-  scene : '',
-  renderer : '',
-  group : '',
-  controls : '',
-  targetRotation : 0,
-  targetRotationOnMouseDown : 0,
-  mouseY : 0,
-  mouseYOnMouseDown : 0,
-  windowHalfY : window.innerHeight/2,
-  extrudeSettings : { amount: 20, bevelEnabled: false, bevelSegments: 0.5, steps: 1, bevelSize: 1, bevelThickness: 1 },
-  scaleFactor : 4,
-  adjustX : 0,
-  adjustY : 0,
-  blocks : [],
-  arenablock : '',
-  arenas : [],
-  clients : [],
-  clock : '',
-  arena : 'D',
-  loadprocess : 0,
+/*jshint esversion: 6 */
 
-  options : {},
+const elmia3d = (function() {
+  let camera,
+  scene,
+  renderer,
+  group,
+  controls,
+  adjustX,
+  adjustY,
+  blocks = [],
+  loadprocess = 0,
+  loadedArenas = [],
+  loadedClients = [],
+  arenablock,
+  options = {};
 
-  init : function(options){
-    this.options = options;
-    if(this.options.container.indexOf('#') > -1){
-      this.options.container = this.options.container.replace('#', '');
-      this.options.container = document.getElementById(this.options.container);
+  const scaleFactor = 4;
+
+
+  const init = function(userOptions){
+    options = userOptions;
+    if(options.container.indexOf("#") > -1){
+      options.container = options.container.replace("#", "");
+      options.container = document.getElementById(options.container);
 
     }else{
-      this.options.container = this.options.container.replace('.', '');
-      this.options.container = document.getElementsByClassName(this.options.container)[0];
+      options.container = options.container.replace(".", "");
+      options.container = document.getElementsByClassName(options.container)[0];
     }
 
-    this.setUp3D();
-  },
+    setUp3D();
+  };
 
-  setUp3D : function(){
-    var _this = this;
-
-    this.clock = new THREE.Clock();
-
+  const setUp3D = function(){
     //Create scene
-    this.scene = new THREE.Scene();
-    this.scene.background = new THREE.Color( 0x458EA8 );
+    scene = new THREE.Scene();
+    scene.background = new THREE.Color( 0x458EA8 );
     //Create camera
-    this.camera = new THREE.PerspectiveCamera( 45, this.options.container.offsetWidth / this.options.container.offsetHeight, 1, 1000 );
-    this.camera.position.set( 0, 300, 600 );
-    this.scene.add( this.camera );
+    camera = new THREE.PerspectiveCamera( 45, options.container.offsetWidth / options.container.offsetHeight, 1, 1000 );
+    camera.position.set( 0, 300, 600 );
+    scene.add( camera );
 
     //Create renderer
-    this.renderer = new THREE.WebGLRenderer( { antialias: true } );
-    this.renderer.setPixelRatio( window.devicePixelRatio );
-    this.renderer.setSize( this.options.container.offsetWidth, this.options.container.offsetHeight );
+    renderer = new THREE.WebGLRenderer( { antialias: true } );
+    renderer.setPixelRatio( window.devicePixelRatio );
+    renderer.setSize( options.container.offsetWidth, options.container.offsetHeight );
 
     //Add light
-    var light = new THREE.PointLight( 0xffffff, 2 );
+    const light = new THREE.PointLight( 0xffffff, 2 );
     light.castShadow = false;
-    this.camera.add( light );
+    camera.add( light );
 
     //Add 3d Group
-    this.group = new THREE.Group();
-    this.group.rotation.x = Math.PI / 2;
-    this.scene.add( this.group );
+    group = new THREE.Group();
+    group.rotation.x = Math.PI / 2;
+    scene.add( group );
 
     //Enamble Controls
-    this.controls = new THREE.OrbitControls( this.camera,  this.renderer.domElement );
-    this.controls.enableDamping = true;
-    this.controls.dampingFactor = 0.6;
-    this.controls.update();
+    controls = new THREE.OrbitControls( camera,  renderer.domElement );
+    controls.enableDamping = true;
+    controls.dampingFactor = 0.6;
+    controls.update();
 
-    this.options.container.appendChild( this.renderer.domElement );
+    options.container.appendChild( renderer.domElement );
 
-    this.loadData(function(response, ){
-      _this.processArenas(JSON.parse(response).ArrayOfHallCordinates.HallCordinates);
-    }, 'data-arenas.json');
+    loadData(function(response){
+      processArenas(JSON.parse(response).ArrayOfHallCordinates.HallCordinates);
+    }, "data-arenas.json");
 
-    this.loadData(function(response){
-      _this.processClients(JSON.parse(response).StandCordinates);
-    }, 'data-17130.json');
+    loadData(function(response){
+      processClients(JSON.parse(response).StandCordinates);
+    }, "data-17130.json");
 
-    this.animate();
+    animate();
 
-  },
+  };
 
-  changeArena : function(arena){
-    this.group.remove(this.options.arenablock);
-
-    while(this.blocks.length > 0){
-      this.group.remove( this.blocks[0].obj );
-      this.blocks.splice(0, 1);
-    }
-    this.render();
-
-    this.options.arena = arena;
-    this.drawArena();
-    this.drawClients();
-  },
-
-  fireEvent : function(){
-    new TWEEN.Tween( this.blocks[630670].position ).to( { z: -40 }, 500 ).easing(
- TWEEN.Easing.Cubic.Out ).start();
-    new TWEEN.Tween( this.blocks[630670].scale ).to( { z: 2 }, 500 ).easing(
- TWEEN.Easing.Cubic.Out ).start();
-  },
-
-  loadData : function(callback ,src){
-
-    var xobj = new XMLHttpRequest();
+  const loadData = function(callback ,src){
+    const xobj = new XMLHttpRequest();
      xobj.overrideMimeType("application/json");
-     xobj.open('GET', src, true);
+     xobj.open("GET", src, true);
      xobj.onreadystatechange = function() {
          if (xobj.readyState == 4 && xobj.status == "200") {
              callback(xobj.responseText);
          }
      };
      xobj.send(null);
-  },
+  };
 
-  processClients : function(data){
-    for(var i = 0; i < data.length; i++) {
-      var obj = data[i];
-      if(!this.clients[obj.HallName]) this.clients[obj.HallName] = [];
-      this.clients[obj.HallName].push(obj);
+  const processClients = function(data){
+    for (let obj of data) {
+      if(!loadedClients[obj.HallName]) loadedClients[obj.HallName] = [];
+      loadedClients[obj.HallName].push(obj);
     }
 
-    this.addToQue();
-  },
+    addToQue();
+  };
 
-  processArenas : function(data){
-    for(var i = 0; i < data.length; i++) {
-      this.arenas[data[i].HallName] = [];
-      this.arenas[data[i].HallName]['p'] = data[i].Points['d3p1:string'];
-      this.arenas[data[i].HallName]['c'] = data[i].Center.split(', ');
-      ;
+  const processArenas = function(data){
+    for (let obj of data) {
+      loadedArenas[obj.HallName] = [];
+      loadedArenas[obj.HallName].p = obj.Points["d3p1:string"];
+      loadedArenas[obj.HallName].c = obj.Center.split(", ");
     }
 
-    this.addToQue();
-  },
+    addToQue();
+  };
 
-  addToQue : function(){
-    this.loadprocess++;
+  const addToQue = function(){
+    loadprocess++;
 
-    if(this.loadprocess == 2){
-      this.drawArena();
-      this.drawClients();
+    if(loadprocess == 2){
+      drawArena();
+      drawClients();
     }
-  },
+  };
 
-  drawArena : function(data){
-    var shape = new THREE.Shape(),
-    points = this.arenas[this.options.arena]['p'],
-    startVal = points[points.length - 1].split(', ');
+  const drawArena = function(data){
+    const shape = new THREE.Shape(),
+    points = loadedArenas[options.arena].p,
+    startVal = points[points.length - 1].split(", ");
 
-    this.adjustX = this.arenas[this.options.arena]['c'][0] * this.scaleFactor,
-    this.adjustY = this.arenas[this.options.arena]['c'][1] * this.scaleFactor;
+    adjustX = (loadedArenas[options.arena].c[0] * scaleFactor);
+    adjustY = (loadedArenas[options.arena].c[1] * scaleFactor);
 
-    shape.moveTo((parseInt(startVal[0]) * this.scaleFactor)-this.adjustX , (parseInt(startVal[1]) * this.scaleFactor)-this.adjustY);
+    shape.moveTo((parseInt(startVal[0]) * scaleFactor)-adjustX , (parseInt(startVal[1]) * scaleFactor)-adjustY);
 
-    for(var j = 0; j < points.length; j++){
-      var pointVal = points[j].split(', ');
-      shape.lineTo( (parseInt(pointVal[0]) * this.scaleFactor) - this.adjustX, (parseInt(pointVal[1]) * this.scaleFactor) - this.adjustY );
+    for(let point of points){
+      var pointVal = point.split(", ");
+      shape.lineTo( (parseInt(pointVal[0]) * scaleFactor) - adjustX, (parseInt(pointVal[1]) * scaleFactor) - adjustY );
     }
 
-    var geometry = new THREE.ExtrudeGeometry( shape, { amount: 5, bevelEnabled: false, bevelSegments: 0, steps: 1, bevelSize: 1, bevelThickness: 1 } ),
+    const geometry = new THREE.ExtrudeGeometry( shape, { amount: 5, bevelEnabled: false, bevelSegments: 0, steps: 1, bevelSize: 1, bevelThickness: 1 } ),
     material = new THREE.MeshPhongMaterial( {
       color: 0x05202E,
       transparent: true,
@@ -179,74 +146,111 @@ window.elmia3d = {
     arena.castShadow = false;
     arena.receiveShadow = false;
 
-    this.options.arenablock = arena;
+    arenablock = arena;
 
-    this.group.add(arena);
-  },
+    group.add(arena);
+  };
 
-  drawClients : function(){
-    for(var i = 0; i < this.clients[this.options.arena].length; i++) {
-        var obj = this.clients[this.options.arena][i],
-        shape = new THREE.Shape(),
-        points = obj.Points.string;
+  const drawClients = function(){
+    for(let obj of loadedClients[options.arena]){
+        const shape = new THREE.Shape();
+        const points = obj.Points.string;
+        const startVal = points[points.length - 1].split(", ");
 
-        startVal = points[points.length - 1].split(', ');
+        shape.moveTo((parseInt(startVal[0]) * scaleFactor) - adjustX , (parseInt(startVal[1]) * scaleFactor) - adjustY );
 
-        shape.moveTo((parseInt(startVal[0]) * this.scaleFactor) - this.adjustX , (parseInt(startVal[1]) * this.scaleFactor) - this.adjustY );
-
-        for(var j = 0; j < points.length; j++){
-          var pointVal = points[j].split(', ');
-          shape.lineTo( (parseInt(pointVal[0]) * this.scaleFactor) - this.adjustX , (parseInt(pointVal[1]) * this.scaleFactor) - this.adjustY );
+        for(let point of points){
+          var pointVal = point.split(", ");
+          shape.lineTo( (parseInt(pointVal[0]) * scaleFactor) - adjustX , (parseInt(pointVal[1]) * scaleFactor) - adjustY );
         }
 
-        this.addShape( shape, obj.BlockId);
+        addShape( shape, obj.BlockId);
 
     }
-    this.render();
-  },
+    render();
+  };
 
-  addShape : function(shape, _id){
-    var geometry = new THREE.ExtrudeGeometry( shape, this.extrudeSettings ),
-    material = new THREE.MeshDepthMaterial( {
+  const addShape = function(shape, _id){
+    const geometry = new THREE.ExtrudeGeometry( shape, { amount: 20, bevelEnabled: false, bevelSegments: 0.5, steps: 1, bevelSize: 1, bevelThickness: 1 } );
+    const material = new THREE.MeshDepthMaterial( {
       transparent: true,
       opacity: 1,
       polygonOffset: true,
       polygonOffsetFactor: 1, // positive value pushes polygon further away
       polygonOffsetUnits: 1
-    } ),
-    mesh = new THREE.Mesh( geometry,  material );
+    } );
+    const mesh = new THREE.Mesh( geometry,  material );
+
     mesh.position.set( 0, 0, -20 );
     mesh.castShadow = true;
     mesh.receiveShadow = true;
     mesh.name = _id;
-    this.blocks.push({
-      'id' : _id,
-      'obj' : mesh
+
+    blocks.push({
+      "id" : _id,
+      "obj" : mesh
     });
-    this.group.add( mesh );
+    group.add( mesh );
 
-    var geo = new THREE.EdgesGeometry( mesh.geometry ); // or WireframeGeometry
-    var mat = new THREE.LineBasicMaterial( { color: 0x0C2333, linewidth: 2 } );
-    var wireframe = new THREE.LineSegments( geo, mat );
-    mesh.add( wireframe );
-  },
+    mesh.add( new THREE.LineSegments( new THREE.EdgesGeometry( mesh.geometry ), new THREE.LineBasicMaterial( { color: 0x0C2333, linewidth: 2 } )));
+  };
 
-  animate : function(){
-    var _this = this;
-    var t = this.clock.getElapsedTime();
+  const getBlockById = function(_id){
+    let block;
+    for (let obj of blocks){
+      if(obj.id == _id){
+        block = obj;
+        break;
+      }
+    }
+
+    return block;
+  }
+
+  const animate = function(){
     requestAnimationFrame( function(){
-      _this.animate();
+      animate();
     } );
 
     TWEEN.update();
-    // required if controls.enableDamping or controls.autoRotate are set to true
-    this.controls.update();
+    controls.update();
 
-    this.render();
-  },
+    render();
+  };
 
- render : function() {
-    //group.rotation.x += ( targetRotation - group.rotation.x ) * 0.05;
-    this.renderer.render( this.scene, this.camera );
-  }
-}
+  const render = function() {
+    renderer.render( scene, camera );
+  };
+
+  const changeArena = function(arena){
+    group.remove(arenablock);
+
+    while(blocks.length > 0){
+      group.remove( blocks[0].obj );
+      blocks.splice(0, 1);
+    }
+    render();
+
+    options.arena = arena;
+    drawArena();
+    drawClients();
+  };
+
+  const showBlocksById = function(arr){
+    for(let _id of arr){
+      let block = getBlockById(_id);
+
+      if(block){
+        new TWEEN.Tween( block.obj.position ).to( { z: -40 }, 500 ).easing(
+     TWEEN.Easing.Cubic.Out ).start();
+        new TWEEN.Tween( block.obj.scale ).to( { z: 2 }, 500 ).easing(
+     TWEEN.Easing.Cubic.Out ).start();
+      }
+    }
+  };
+
+  return {
+    init : init,
+    showBlocksById : showBlocksById
+  };
+})();
